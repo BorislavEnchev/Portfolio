1bd72e97eac641e458129e4974ddd54d42ca58fd
Borislav Enchev <thugluv@abv.bg>
2024-11-27 12:26:46 +0200

    unused css deleted

--- src/main.jsx
+++ src/main.jsx
@@ -1,6 +1,5 @@
 import { StrictMode } from 'react'
 import { createRoot } from 'react-dom/client'
-/*import './index.css'*/
 import App from './App.jsx'
 import p5 from 'p5';
 import { setup, draw, windowResized } from './snowflakes.js';
