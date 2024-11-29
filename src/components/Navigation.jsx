92383f46d423214f96fbbc0feff4ea298b4e44ac
Borislav Enchev <thugluv@abv.bg>
2024-11-29 15:25:39 +0200

    projects dynamically added in the navigation menu

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -1,21 +1,24 @@
 import './Navigation.less';
+import { PROJECTS } from '../projects.js';
 
 export default function Navigation({ onMenuClick, onProjectClick }) {
-    return (<>
+    return (
         <nav className="menu">
             <ol>
                 <li className="menu-item"><a href="#0" onClick={() => onMenuClick('Home')}>Home</a></li>
                 <li className="menu-item">
                     <a href="#0" onClick={() => onMenuClick('Projects')}>Projects</a>
                     <ol className="sub-menu">
-                        <li className="menu-item"><a href="#0" onClick={() => onProjectClick('npilookup')}>NPI Lookup</a></li>
-                        <li className="menu-item"><a href="#0" onClick={() => onProjectClick('morningmed')}>MorningMed</a></li>
-                        <li className="menu-item"><a href="#0" onClick={() => onProjectClick('studytracker')}>Study Tracker</a></li>
+                        {Object.keys(PROJECTS).map((projectKey) => (
+                            <li className="menu-item" key={projectKey}>
+                                <a href="#0" onClick={() => onProjectClick(projectKey)}>{PROJECTS[projectKey].title}</a>
+                            </li>
+                        ))}
                     </ol>
                 </li>
                 <li className="menu-item"><a href="#0" onClick={() => onMenuClick('About')}>About</a></li>
                 <li className="menu-item"><a href="#0" onClick={() => onMenuClick('Contact')}>Contact</a></li>
             </ol>
-        </nav>        
-    </>)
+        </nav>
+    );
 }
