acab7cbc34782fa558047e4c951ed750fc2ec4ee
Borislav Enchev <thugluv@abv.bg>
2024-12-09 14:20:52 +0200

    font awesome fix

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -5,19 +5,19 @@ export default function Navigation({ onMenuClick, onProjectClick }) {
     return (
         <nav className="menu">
             <ol>
-                <li className="menu-item"><a href="#0" onClick={() => onMenuClick('Home')}>Home</a></li>
+                <li className="menu-item"><a href="#" onClick={() => onMenuClick('Home')}>Home</a></li>
                 <li className="menu-item">
-                    <a href="#0" onClick={() => onMenuClick('Projects')}>Projects</a>
+                    <a href="#" onClick={() => onMenuClick('Projects')}>Projects</a>
                     <ol className="sub-menu">
                         {Object.keys(PROJECTS).map((projectKey) => (
                             <li className="menu-item" key={projectKey}>
-                                <a href="#0" onClick={() => onProjectClick(projectKey)}>{PROJECTS[projectKey].title}</a>
+                                <a href="#" onClick={() => onProjectClick(projectKey)}>{PROJECTS[projectKey].title}</a>
                             </li>
                         ))}
                     </ol>
                 </li>
-                <li className="menu-item"><a href="#0" onClick={() => onMenuClick('About')}>About</a></li>
-                <li className="menu-item"><a href="#0" onClick={() => onMenuClick('Contact')}>Contact</a></li>
+                <li className="menu-item"><a href="#" onClick={() => onMenuClick('About')}>About</a></li>
+                <li className="menu-item"><a href="#" onClick={() => onMenuClick('Contact')}>Contact</a></li>
             </ol>
         </nav>
     );
