55c8d0058916e89fccd0b4e3a91cc26a172ce49a
Borislav Enchev <thugluv@abv.bg>
2024-12-27 11:13:44 +0200

    GitHub text updated

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -3,6 +3,6 @@ export default function Contact() {
         <h2>Borislav Enchev Enchev</h2>
         <p>Telephone: +35984902041</p>
         <p>Email: borkokiborko@gmail.com</p>
-        <p><a href="https://github.com/BorislavEnchev" class="plain-link" target="_blank">Github: github.com/BorislavEnchev</a></p>      
+        <p><a href="https://github.com/BorislavEnchev" class="plain-link" target="_blank">GitHub: /BorislavEnchev</a></p>      
     </>)
 }
