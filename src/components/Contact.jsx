3078467938a059aa99a31d7146e3eff185763229
Borislav Enchev <thugluv@abv.bg>
2025-01-03 13:17:44 +0200

    update phone

--- src/components/Contact.jsx
+++ src/components/Contact.jsx
@@ -1,7 +1,7 @@
 export default function Contact() {
     return (<>
         <h2>Borislav Enchev Enchev</h2>
-        <p>Telephone: +35984902041</p>
+        <p>Telephone: +359 884902041</p>
         <p>Email: borkokiborko@gmail.com</p>
         <p><a href="https://github.com/BorislavEnchev" class="plain-link" target="_blank">GitHub: /BorislavEnchev</a></p>      
     </>)
