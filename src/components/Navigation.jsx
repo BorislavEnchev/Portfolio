878e2310f5d4d3b10ed72485b20b419eabf63f7a
Borislav Enchev <thugluv@abv.bg>
2025-01-03 13:35:52 +0200

    clean url

--- src/components/Navigation.jsx
+++ src/components/Navigation.jsx
@@ -2,31 +2,37 @@ import './Navigation.less';
 import { PROJECTS } from '../projects.js';
 
 export default function Navigation({ onMenuClick, onProjectClick }) {
-    const handleMenuClick = (menu) => () => onMenuClick(menu);
-    const handleProjectClick = (projectKey) => () => onProjectClick(projectKey);
+    const handleMenuClick = (menu) => (event) => {
+        event.preventDefault();
+        onMenuClick(menu);
+    };
+    const handleProjectClick = (projectKey) => (event) => {
+        event.preventDefault();
+        onProjectClick(projectKey);
+    };
     const projectEntries = Object.entries(PROJECTS);
 
     return (
         <nav className="menu">
             <ol>
                 <li className="menu-item">
-                    <a href="#" onClick={handleMenuClick('Home')}>Home</a>
+                    <a href="/" onClick={handleMenuClick('Home')}>Home</a>
                 </li>
                 <li className="menu-item">
-                    <a href="#" onClick={handleMenuClick('Projects')}>Work</a>
+                    <a href="/" onClick={handleMenuClick('Projects')}>Work</a>
                     <ol className="sub-menu">
                         {projectEntries.map(([projectKey, project]) => (
                             <li className="menu-item" key={projectKey}>
-                                <a href="#" onClick={handleProjectClick(projectKey)}>{project.title}</a>
+                                <a href="/" onClick={handleProjectClick(projectKey)}>{project.title}</a>
                             </li>
                         ))}
                     </ol>
                 </li>
                 <li className="menu-item">
-                    <a href="#" onClick={handleMenuClick('About')}>About</a>
+                    <a href="/" onClick={handleMenuClick('About')}>About</a>
                 </li>
                 <li className="menu-item">
-                    <a href="#" onClick={handleMenuClick('Contact')}>Contact</a>
+                    <a href="/" onClick={handleMenuClick('Contact')}>Contact</a>
                 </li>
             </ol>
         </nav>
--- src/components/Projects.jsx
+++ src/components/Projects.jsx
@@ -3,13 +3,18 @@ import styles from './Projects.module.css';
 import { PROJECTS } from '../projects.js';
 
 export default function Projects({ onProjectClick }) {
+    const handleProjectClick = (projectName) => (event) => {
+        event.preventDefault();
+        onProjectClick(projectName);
+    };
+
     return (
         <>
             <h1>Projects and Apps</h1>
             <ul className={styles.list}>
                 {Object.keys(PROJECTS).map((projectName) => (
                     <li key={projectName}>
-                        <a href="#" onClick={() => onProjectClick(projectName)}>
+                        <a href="/" onClick={handleProjectClick(projectName)}>
                             <h2>{PROJECTS[projectName].title}</h2>
                         </a>
                     </li>
@@ -18,3 +23,4 @@ export default function Projects({ onProjectClick }) {
         </>
     );
 }
+
